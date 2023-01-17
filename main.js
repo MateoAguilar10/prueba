2cd9b820d5d42ddff3645430a8a78d761995746d
MateoAguilar10 <mateoaguilarramirez6@gmail.com>
2023-01-17 15:39:16 -0500

    agregue un nuevo parrafo

---
+++ main.js
@@ -0,0 +1,19 @@
+// var edad
+
+// console.log("ingrese su edad");
+
+// if(edad == 19){
+//     console.log("Su edad es correcta");
+// }else{
+//     console.log("Su usuario es incorrecto");
+// }
+
+// const numeroUno
+
+function calculadora(){
+    const numeroUno = document.getElementById('numeroUno');
+    const numeroDos = document.getElementById('numeroDos');
+    const resultado = document.getElementById('resultado');
+    const valores = parseInt(numeroUno.value) + parseInt(numeroDos.value) + parseInt(resultado);
+
+}
